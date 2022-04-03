e59e88020f6a1ee8486d194a1cc3b6494f6daf24
Jesse Ojih <jcinvent05@gmail.com>
2022-04-03 11:38:59 +0100

    Initial commit

--- controllers/trades.js
+++ controllers/trades.js
@@ -1,1 +1,34 @@
+const Trade = require("../models/trades");
 
+exports.getAllTrades = async (req, res, next) => {
+  const trades = await Trade.findAll({
+    order: [["id", "ASC"]],
+  });
+
+  return res.status(200).json(trades);
+};
+
+exports.createTrade = async (req, res, next) => {
+  const trade = await Trade.create({ ...req.body });
+
+  return res.status(201).json(trade.get());
+};
+
+exports.findTrade = async (req, res, next) => {
+  const id = req.params.id;
+
+  const trade = await Trade.findOne({
+    where: { id },
+    order: [["id", "ASC"]],
+  });
+
+  if (!trade) {
+    return res.status(404).send("ID not found");
+  }
+
+  return res.status(200).json(trade);
+};
+
+exports.throwError = async (req, res, next) => {
+  return res.status(405).send("error");
+};
--- routes/trades.js
+++ routes/trades.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require("express");
+const {
+  getAllTrades,
+  createTrade,
+  findTrade,
+  throwError,
+} = require("../controllers/trades");
 const router = express.Router();
 
+router.get("/", getAllTrades);
+
+router.post("/", createTrade);
+
+router
+  .route("/:id")
+  .get(findTrade)
+  .delete(throwError)
+  .patch(throwError)
+  .put(throwError);
 
 module.exports = router;
